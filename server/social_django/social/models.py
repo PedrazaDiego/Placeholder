ada390aefe0d50c5aea33265bad645e57d23ae85
PedrazaDiego <skapet01@gmail.com>
2022-02-07 12:27:43 -0500

    seeded somw dummy data

--- server/social_django/social/models.py
+++ server/social_django/social/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.CharField(max_length=256)
+    image = models.TextField()
+
+    def __str__(self):
+        return 'post'
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return 'like'
--- server/social_django/social_django/settings.py
+++ server/social_django/social_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social',
+    'django_seed',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'social_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social',
+        'USER': 'socialuser',
+        'PASSWORD': 'social',
+        'HOST': 'localhost'
     }
 }
 
