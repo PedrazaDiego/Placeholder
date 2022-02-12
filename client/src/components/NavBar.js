453a5fafb878b07cb01c0268f1ecf55715eefefa
PedrazaDiego <skapet01@gmail.com>
2022-02-12 11:25:24 -0500

    imported material ui, testing navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { AppBar, Toolbar, Typography } from '@mui/material'
+
 
 const NavBar = () => {
     return (
-        <div >
-            <nav className='nav'>
-                <Link to='/'> Landing </Link>
-                <Link to='/profile'> Profile </Link>
-            </nav>
-        </div>
+        <AppBar >
+            <Toolbar>
+                <nav className='nav'>
+                    <Link to='/'> Landing </Link>
+                    <Link to='/profile'> Profile </Link>
+                </nav>
+            </Toolbar>
+        </AppBar>
     )
 }
 
